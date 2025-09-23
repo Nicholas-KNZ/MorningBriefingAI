4ef8047dd48f698fbb2641bb09a83d1ad1ed33ab
Nicholas  Kunze <nicholas.marcel.kunze@gmail.com>
2025-09-23 20:29:31 +0200

    Requirements

--- main.py
+++ main.py
@@ -41,6 +41,9 @@ def process_input():
 def health_check():
     return jsonify({'status': 'ok'})
 
+import os
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
     app.run(host="0.0.0.0", port=port)
+
